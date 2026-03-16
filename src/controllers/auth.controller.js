e4b82ae77381e523c348ec2723fd40a1dc8850f0
StrawHat-Luffyyy <krishmac1209@gmail.com>
2026-03-16 23:36:53 +0530

    feat: implement login and logout functionality in auth controller and routes

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -32,3 +32,47 @@ export const register = async (req, res, next) => {
     next(error);
   }
 };
+
+// @desc    Login user
+// @route   POST /api/auth/login
+// @access  Public
+export const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res
+        .status(401)
+        .json({ success: false, error: "Please provide email and password" });
+    }
+    const user = await prisma.user.findUnique({ where: email });
+    if (!user) {
+      return res
+        .status(401)
+        .json({ success: false, error: "Invalid credentials" });
+    }
+    const isMatch = await bcrypt.compare(password, user.passwordHash);
+    if (!isMatch) {
+      return res
+        .status(401)
+        .json({ success: false, error: "Invalid credentials" });
+    }
+    sendTokenResponse(user, 200, res);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// @desc    Log user out / clear cookie
+// @route   POST /api/auth/logout
+// @access  Public
+export const logout = async (req, res, next) => {
+  try {
+    res.cookie("token", "none", {
+      expires: new Date(Date.now() + 10 * 1000),
+      httpOnly: true,
+    });
+    res.status(200).json({ success: true, data: {} });
+  } catch (error) {
+    next(error);
+  }
+};
